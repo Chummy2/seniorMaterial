aa6eb4e87ae55d12b78dab15a56796b446ede79d
Chummy2 <justinbrucken1@gmail.com>
2023-08-30 21:18:25 -0500

    worek

---
+++ Java/homework/forloop.java
@@ -0,0 +1,27 @@
+public class forloop {
+    public static void main(String[] args) {
+       int x=0;
+        for(int p=0;p!=3;p++){
+            for(int w=2;w>=0;w-=1){
+            if(x<8){
+                System.out.print(w+",");
+                x=x+1;
+            }
+            else {
+                System.out.println(w);
+            }
+
+        }
+}
+int x2=0;
+        for(int p2=10;p2!=3;p2--){
+            for(int w2=2;w2>=0;w2-=1){
+            if(x2<8){
+                System.out.print(w2+",");
+                x2=x2+1;
+            }
+            else {
+                System.out.print(w2);
+            }
+}
+}}}
