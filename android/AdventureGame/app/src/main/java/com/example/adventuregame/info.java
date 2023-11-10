bb045c935774ca3538dda6bc3ca365f75cc50688
Chummy2 <justinbrucken1@gmail.com>
2023-11-09 22:55:26 -0600

    wor

--- android/AdventureGame/app/src/main/java/com/example/adventuregame/info.java
+++ android/AdventureGame/app/src/main/java/com/example/adventuregame/info.java
@@ -2,7 +2,10 @@ package com.example.adventuregame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class info extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class info extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        Button mainButton = findViewById(R.id.mainButton);
+        mainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an Intent to start the InfoActivity
+                Intent main = new Intent(info.this, MainActivity.class);
+                startActivity(main);
+            }
+        });
     }
 }
